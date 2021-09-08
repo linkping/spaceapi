ec27b6cb8abf973c82f11eeb3a5519be9316b8dc
Lars-Magnus Skog <ralphtheninja@riseup.net>
2021-09-08 22:11:44 +0200

    First attempt at json with real information

--- index.js
+++ index.js
@@ -15,7 +15,32 @@ async function start () {
 
   app.route('/').get(async (req, res) => {
     res.json({
-      beep: 'boop'
+      api_compatibility: ['14'],
+      space: 'LINKping',
+      // TODO update logo, using shackspace logo for now just to test json schema
+      logo: 'http://rescue.shackspace.de/images/logo_shack_brightbg_highres.png',
+      // TODO returning docs page for now, change to real homepage later
+      url: 'https://docs.linkping.org',
+      location: {
+        address: 'Snickaregatan 16, Linköping, Sweden',
+        // TODO update using coordinates from https://wiki.hackerspaces.org/LINKPing
+        lat: 48.777,
+        lon: 9.236,
+        timezone: 'SE'
+      },
+      contact: {
+        email: 'ralphtheninja@riseup.net',
+        irc: 'irc://irc.libera.chat/#linkping'
+      },
+      state: {
+        icon: {
+          // TODO replace icons
+          open: 'http://shackspace.de/sopen.gif',
+          closed: 'http://shackspace.de/sopen.gif'
+        },
+        // TODO make dynamic
+        open: false
+      }
     })
   })
 
